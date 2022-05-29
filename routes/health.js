1f570bfa090d191009ca8050aaef1d6edd569837
Panthy <Fahad.alhatshami@hotmail.com>
2022-05-29 11:50:12 +0200

    simple health check

--- routes/health.js
+++ routes/health.js
@@ -3,6 +3,10 @@ var express = require("express");
 const router = express.Router();
 
 // checks the health of the server
-router.get("/", async function (req, res) {});
+router.get("/", async function (req, res) {
+  res.status(200).json({
+    status: "OK",
+  });
+});
 
 module.exports = { healthRouter: router };
